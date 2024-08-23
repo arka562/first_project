b0b5fa0b9fc097d910795ea1cd8350c9f9415d2a
Arkaprava <arka792002@gmail.com>
2024-08-23 09:38:24 +0530

    middlewares developed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import {v2} from "cloudinary"
+import fs from 'fs'
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINATY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary= async function(localFilePath) {
+  try{
+    if(!localFilePath) return null
+    const response=cloudinary.uploader.upload(localFilePath,{resource_type:auto})
+    console.log("the file get connected to cloudinary",response.url)
+    return response
+  }
+  catch(error){
+    fs.unlinkSync(localFilePath) //remove the locally saved temporary file as file failed to get uploaded
+
+  }
+  
+}
+
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+
+    export {uploadOnCloudinary}
