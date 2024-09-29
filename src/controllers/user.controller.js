c02d1d257b3b9360b8edf36aa4849d0ae99bcb2e
Arkaprava <arka792002@gmail.com>
2024-09-29 16:52:57 +0530

    last commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -157,7 +157,7 @@ const loginUser = asyncHandler(async (req, res) =>{
     )
 
 })
-/*
+
 const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
@@ -493,4 +493,4 @@ export {
     updateUserCoverImage,
     getUserChannelProfile,
     getWatchHistory
-} */
+} 
